f00f7f97688ff7a09cc4043d22481791c3cb33ef
Tiago Pereira <tiagopereiralopesdossantos@gmail.com>
2024-08-12 21:46:28 -0300

    first commit

---
+++ exercicio.ts
@@ -0,0 +1,9 @@
+// Função de multiplicação
+function funcaoDeMultiplicacao(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function funcaoDeSaudacao(seuNome: string): string {
+    return "Olá " + seuNome;
+}
